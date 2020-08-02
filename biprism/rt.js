04c10e9b6db9929a644bf53daf02e8201579554c
ashadulhalder <56104577+ashadulhalder@users.noreply.github.com>
2020-08-02 11:14:55 +0530

    Add files via upload

---
+++ biprism/rt.js
@@ -0,0 +1,22 @@
+$(function(){
+  let slider = $('input.image-rotate1'),
+      image = $('#setup');
+  
+  slider.on('change mousemove', function(){
+     image.css('transform', 'rotate(' + $(this).val() + 'deg)')
+  });
+  
+  
+})
+
+$(function(){
+  let slider = $('input.image-rotate2'),
+      image = $('#image2');
+  
+  slider.on('change mousemove', function(){
+     image.css('transform', 'rotate(' + $(this).val() + 'deg)')
+  });
+  
+  
+})
+
